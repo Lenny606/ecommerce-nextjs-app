e436a09089db911487555d9035281f13a09bc3ec
Tomas Kravcik <root@Laptop-i7>
2024-12-28 12:24:58 +0100

    edit dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,18 +1,38 @@
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
 import {string} from "postcss-selector-parser";
+import db from "@/db/db";
 
-export default function AdminDashboard() {
+async function getSalesData() {
+
+    const data = await db.order.aggregate({
+        _sum: {
+            price: true
+        },
+        _count: true
+    })
+
+    return {
+        amount: data._sum.price || 0,
+        numberOfSales: data._count
+    }
+}
+
+
+export default async function AdminDashboard() {
+
+    const data = await getSalesData()
+
     return (
         <div className={'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'}>
-            <DashboardCard title={"Sales"} subtitle={"some description "} content={'some description'}/>
+            <DashboardCard title={"Sales"} subtitle={data.numberOfSales} content={data.amount}/>
         </div>
     );
 };
 
 type DashboardCardProps = {
     title: string
-    subtitle: string
-    content: string
+    subtitle: string | number
+    content: string | number
 }
 
 export function DashboardCard({title, subtitle, content}: DashboardCardProps) {
