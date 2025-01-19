c912758c7917a5bb447e5d58f40332a0c2830944
Tomas Kravcik <kravcik@procorp.cz>
2025-01-19 20:04:54 +0100

    checkout form - styles

--- src/components/CheckoutForm.tsx
+++ src/components/CheckoutForm.tsx
@@ -13,11 +13,13 @@ export default function CheckoutForm({product, clientSecret}: CheckoutProps) {
     const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_KEY as string)
 
     return (
-        //ELement is kind of context wrapper
-        <Elements stripe={stripePromise} options={{clientSecret}}>
-            <Form/>
+        <div className={'max-w-5xl w-full mx-auto space-y-8'}>
+            {/*//ELement is kind of context wrapper*/}
+            <Elements stripe={stripePromise} options={{clientSecret}}>
+                <Form/>
+            </Elements>
+        </div>
 
-        </Elements>
     )
 }
 
