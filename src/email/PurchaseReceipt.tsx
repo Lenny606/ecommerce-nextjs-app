bd978c97fa59170841b56d39a5dd5df4d6de77f9
Tomas Kravcik <kravcik@procorp.cz>
2025-01-26 13:16:22 +0100

    email

--- src/email/PurchaseReceipt.tsx
+++ src/email/PurchaseReceipt.tsx
@@ -1,25 +1,39 @@
 import {Body, Container, Head, Heading, Html, Preview, Tailwind} from "@react-email/components";
 import OrderInformations from "@/email/_components/OrderInformations";
 
-type PuchaseReceiptEmailProps =
-    {
-        product: {
-            name: string
-        }
-    }
+type PuchaseReceiptEmailProps = {
+    product: {
+        name: string
+    },
+    order: { id: string, createdAt: Date, price: number },
+    downloadId: string
 
+}
 
-export default function PurchaseReceipt({product}: PuchaseReceiptEmailProps) {
+// PuchaseReceiptEmailProps.PreviewProps = {
+//     product: {
+//         name: "Sample Product"
+//     },
+//     order: {
+//         id: crypto.randomUUID(),
+//         createdAt: new Date(),
+//         price: 100
+//     },
+// downloadId: {
+// id: crypto.randomUUID(),
+// } satisfies PuchaseReceiptEmailProps
+
+export default function PurchaseReceipt({product, order, downloadId}: PuchaseReceiptEmailProps) {
     return (
         <Html>
-            <Preview> Download {product.name}</Preview>
+            <Preview> Download {product?.name ?? "name"}</Preview>
             {/*Declaration to use tailwind as styles - needs custom config */}
             <Tailwind>
                 <Head></Head>
                 <Body className={"bg-white font-sans"}>
                     <Container>
                         <Heading> Purchase receipt</Heading>
-                        <OrderInformations/>
+                        <OrderInformations order={order} product={product} downloadId={downloadId}/>
                     </Container>
                 </Body>
             </Tailwind>
--- src/email/_components/OrderInformations.tsx
+++ src/email/_components/OrderInformations.tsx
@@ -1,8 +1,42 @@
+import {Column, Row, Text, Section, Img, Button} from '@react-email/components'
 import React from 'react'
 
-export default function OrderInformations() {
+type OIEmailProps = {
+    order: { id: string, createdAt: string, price: number }
+    product: { imagePath: string }
+    downloadId: string
+}
+export default function OrderInformations({order, product, downloadId}: OIEmailProps) {
     return (
-        <div>OrderInformations</div>
+        <>
+            <Section>
+                <Row>
+                    <Column>
+                        <Text className={'text-gray-500 whitespace-nowrap text-nowrap mr-4 mb-0'}>
+                            Order ID: {order?.id}
+                        </Text>
+                    </Column>
+                    <Column>
+                        <Text className={'text-gray-500 whitespace-nowrap text-nowrap mr-4 mb-0'}>
+                            Date: {order?.createdAt}
+                            {/*    format date */}
+                        </Text>
+                    </Column>
+                    <Column>
+                        <Text className={'text-gray-500 whitespace-nowrap text-nowrap mr-4 mb-0'}>
+                            Price: {order?.price}
+                        </Text>
+                    </Column>
+                </Row>
+            </Section>
+            <Section className={'border border-solid'}>
+                <Img src={`${process.env.NEXT_PUBLIC_SERVER_URL}${product?.imagePath}`}/>
+                <Button href={`${process.env.NEXT_PUBLIC_SERVER_URL}/download/${downloadId ?? ""}`}>
+                    Download
+                </Button>
+            </Section>
+        {/*    MORE INFO */}
+        </>
     )
 }
 
